1397a1be2c82bd446b81b89341787b7fe15e28dd
dotminic <dominic.mckerl@gmail.com>
2013-07-03 00:00:30 +0200

    added: get_next_line utility
    small function that reads a file line by line.

---
+++ include/get_next_line.h
@@ -0,0 +1,9 @@
+#ifndef __GET_NEXT_LINE_H__
+# define __GET_NEXT_LINE_H__
+
+#define	BUFFER_SIZE	256
+
+/// fd is a valid file descriptor
+char	*get_next_line( int fd );
+
+#endif /* !__GET_NEXT_LINE_H__ */
---
+++ src/get_next_line.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include "get_next_line.h"
+
+/*
+read stream from fd
+return if buffer is full or if char is \n
+ */
+
+char	*get_next_line( int fd )
+{
+  int		i;
+  char		*line;
+  static char	buffer[BUFFER_SIZE];
+  static int	read_pos = 0;
+  static int	bytes_read = 0;
+
+  if (read_pos == bytes_read)
+    {
+      read_pos = 0;
+      if ((bytes_read = read(fd, buffer, BUFFER_SIZE)) == 0)
+	return NULL;
+    }
+  if (!(line = malloc(bytes_read - read_pos + 1)))
+    return NULL;
+  for (i = 0; read_pos < bytes_read; read_pos++, i++)
+    {
+      line[i] = buffer[read_pos];
+      if (buffer[read_pos] == '\n')
+	{
+	  read_pos++;
+	  line[i + 1] = 0;
+	  return line;
+	}
+    }
+  line[i] = 0;
+  return line;
+}
